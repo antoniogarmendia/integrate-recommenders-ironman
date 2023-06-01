09785c5a47c5eb7055c0bea81b32fa2221dd6572
LAPTOP-B5F9U8D1\Antonio <antonio.agj@gmail.com>
2023-06-01 16:23:05 +0200

    #15 Preference Initializer

---
+++ integrate.recommenders.ironman.project/bundles/integrate-recommenders-ironman/src/integrate/recommenders/ironman/wizard/preferences/IronManPreferenceInitializer.java
@@ -0,0 +1,17 @@
+package integrate.recommenders.ironman.wizard.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+
+public class IronManPreferenceInitializer extends AbstractPreferenceInitializer {
+
+	public IronManPreferenceInitializer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initializeDefaultPreferences() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
