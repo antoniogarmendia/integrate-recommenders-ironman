be2f7c70efe9abeee6a9bc8d0d0838dfaba2cee6
LAPTOP-B5F9U8D1\Antonio <antonio.agj@gmail.com>
2023-06-01 15:30:02 +0200

    #13 Ironman Create Project

---
+++ integrate.recommenders.ironman.project/bundles/integrate-recommenders-ironman/src/integrate/recommenders/ironman/wizard/IronManWizard.java
@@ -0,0 +1,26 @@
+package integrate.recommenders.ironman.wizard;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class IronManWizard extends Wizard implements INewWizard {
+
+	public IronManWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
